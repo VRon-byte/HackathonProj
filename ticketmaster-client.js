9b0f858ab8e144707dc25b7fb4f3856cbe01dee5
shreyaswaghode <shreyas.waghode25052008@gmail.com>
2026-09-12 23:30:40 -0500

    Add files via upload

---
+++ ticketmaster-client.js
@@ -0,0 +1,3 @@
+// Ticketmaster Discovery consumer keys are designed for browser requests.
+// Keep this local file out of Git; do not add the consumer secret here.
+window.TICKETMASTER_API_KEY = "ze1buZfxZxydh9aEFhwuvVJ4lZEuo16S";
